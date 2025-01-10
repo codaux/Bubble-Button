565f2c5e7c4268cc63ad74abf99ddaba711a242d
Reza Samani <70468060+codaux@users.noreply.github.com>
2025-01-10 03:53:17 -0700

    DEbug

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let blob1 = createBlob({
 });
 
 function createBlob(options) {
-   
   let points = [];  
   let path = options.element;
   let slice = (Math.PI * 2) / options.numPoints;
@@ -20,10 +19,9 @@ function createBlob(options) {
   
   let tl = gsap.timeline({
     onUpdate: update
-  });  
-  
+  });
+
   for (let i = 0; i < options.numPoints; i++) {
-    
     let angle = startAngle + i * slice;
     let duration = gsap.utils.random(options.minDuration, options.maxDuration);
     
@@ -55,9 +53,7 @@ function createBlob(options) {
   return options;
 }
 
-// Cardinal spline - a uniform Catmull-Rom spline with a tension option
 function cardinal(data, closed, tension) {
-  
   if (data.length < 1) return "M0 0";
   if (tension == null) tension = 1;
   
@@ -65,7 +61,6 @@ function cardinal(data, closed, tension) {
   let path = "M" + data[0].x + " " + data[0].y + " C";
   
   for (let i = 0; i < size; i++) {
-    
     let p0, p1, p2, p3;
     
     if (closed) {
@@ -80,7 +75,7 @@ function cardinal(data, closed, tension) {
       p2 = data[i + 1];
       p3 = i == size - 1 ? p2 : data[i + 2];
     }
-        
+    
     let x1 = p1.x + (p2.x - p0.x) / 6 * tension;
     let y1 = p1.y + (p2.y - p0.y) / 6 * tension;
 
